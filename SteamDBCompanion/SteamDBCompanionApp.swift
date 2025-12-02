ab9a59e69dc147ad326da8eff630763f6a8bf0e0
Alxcgs <oleksandr.herasymchuk@oa.edu.ua>
2025-12-02 16:49:54 +0200

    Initial Commit

---
+++ SteamDBCompanion/SteamDBCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteamDBCompanionApp.swift
+//  SteamDBCompanion
+//
+//  Created by Oleksandr Gerasymchuk on 02.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SteamDBCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
